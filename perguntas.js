7d134e367ba8f27a78523e68fc9f888a52973c0d
3AAHS003 <00001066436034sp@al.educacao.sp.gov.br>
2024-11-26 20:26:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'O que foi a guerra fria?',
+    'período de intensa disputa política, militar, econômica, tecnológica, social e ideológica entre os Estados Unidos e a União Soviética'
+)
+
+criaCartao(
+    'astronomia',
+    'Qual o planeta conhecido por ter aneis?',
+    'Saturno'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é IBGE?',
+    'Instituto Brasileiro de Geografia e Estatística'
+)
+
+criaCartao(
+    'Quimica ',
+    'Qual a formula do acido citrico?',
+    'C₆H₈O₇'
+)
